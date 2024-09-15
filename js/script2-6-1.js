00b42432cf483ab93c7dd1942cd457ff4986d07f
{tatsu-narita} <{nt0604.b@gmail.com}>
2024-09-16 00:42:11 +0900

    update: sample2-6-1.js
    sample-3
    表示中の要素を非表示にする hide()

--- js/script2-6-1.js
+++ js/script2-6-1.js
@@ -11,6 +11,18 @@ $(function(){
 
 /* sample-2 */
 // show()にコールバック関数を設定
+/*
+$(function(){
+    $("button").click(function() { 
+        $("div").show("show", function() {
+            $(this).css("background","blue");
+        });
+    });
+});
+*/
+
+/* sample-3 */
+// 表示中の要素を非表示にする hide()
 
 $(function(){
     $("button").click(function() { 
