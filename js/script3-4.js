fec3e8ee8b474b5f0d20ce08f66ad44c67441545
{tatsu-narita} <{nt0604.b@gmail.com}>
2025-01-01 00:28:48 +0900

    update: script3-4.js
    基本的なタブパネル作成/js

--- js/script3-4.js
+++ js/script3-4.js
@@ -1,4 +1,7 @@
 $(function(){
     $("ul.panel li:not("+$("ul.tab li a.selected").attr("href")+")").hidde();
-    
+    $("ul.tab li a").Click(function(){
+        $("ul.tab li a.selected").removeClass("selected");
+        
+    });
 });
