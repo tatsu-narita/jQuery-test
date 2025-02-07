353e5a521050003680813bf5f1481becdf99bae0
{tatsu-narita} <{nt0604.b@gmail.com}>
2025-02-08 02:23:03 +0900

    update: script3-6.js
    ロールオーバーの基本的な仕組みを作る/js

--- js/script3-6.js
+++ js/script3-6.js
@@ -1,7 +1,14 @@
 $(function(){
     $("img[src='images/jquery.png']").mouseover(function () { 
-        $(this).attr("src","images/jquery._on.png");
+        $(this).attr("src","images/jquery_on.png");
     }).mouseout(function () { 
         $(this).attr("src", "images/jquery.png");                                                                                                    
     });
+
+    $("img[src='images/javascript.png']").mouseover(function () { 
+        $(this).attr("src", "images/javascript_on.png");
+    }).mouseout(function () { 
+        $(this).attr("src", "images/javascript.png");
+    });
+    
 });
