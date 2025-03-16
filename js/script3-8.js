eba03ffe1d0a25b803836142e352659bff383550
{tatsu-narita} <{nt0604.b@gmail.com}>
2025-03-16 22:27:47 +0900

    update: script3-8.js
    Lightbox風モーダルウィンドウ/js

--- js/script3-8.js
+++ js/script3-8.js
@@ -1,7 +1,14 @@
 $(function () {
     $("body").append("<div id='glayLayer'></div><div id='overLayer'></div>");
     $("#glayLayer").click(function () { 
-        
-        
+        $("#glayLayer").click(function () { 
+            $(this).hide();
+            $("#overLayer").hide();
+        });
+        $("a.modal").click(function () { 
+            $("#glayLayer").show();
+            $("#overLayer").show().html("<img src"+$(this).attr("href")+">");
+            return false;
+        });
     });
 });
