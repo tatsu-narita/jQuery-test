92320f77f32ec0c36b2fcc360e14dac8c1b5bf71
{tatsu-narita} <{nt0604.b@gmail.com}>
2025-04-11 13:33:24 +0900

    update: script3-10.js
    バリデーション付きメールフォーム/js

--- js/script3-10.js
+++ js/script3-10.js
@@ -3,5 +3,8 @@ $(function() {
         //エラーの初期化
         $("p.error").remove();
         $("dl dd").removeClass("error");
+        $("input[type='text'].validate, textarea.validate").each(function () { 
+            //必須項目チェック
+        });
     });
 });
